19de68b4d3c7ebde6fb6a959a34c32c26cfe4600
kartiktechmero <kartikptechmero@gmail.com>
2025-10-13 11:49:51 +0530

    NEW: pint

--- app/Filament/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -3,8 +3,8 @@
 namespace App\Filament\Resources\Users\Schemas;
 
 use Filament\Forms\Components\DateTimePicker;
-use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
 
 class UserForm
@@ -19,15 +19,15 @@ class UserForm
                     ->label('Email address')
                     ->email()
                     ->required(),
-//                DateTimePicker::make('email_verified_at'),
+                //                DateTimePicker::make('email_verified_at'),
                 TextInput::make('password')
                     ->password()
                     ->required(),
-//                Textarea::make('two_factor_secret')
-//                    ->columnSpanFull(),
-//                Textarea::make('two_factor_recovery_codes')
-//                    ->columnSpanFull(),
-//                DateTimePicker::make('two_factor_confirmed_at'),
+                //                Textarea::make('two_factor_secret')
+                //                    ->columnSpanFull(),
+                //                Textarea::make('two_factor_recovery_codes')
+                //                    ->columnSpanFull(),
+                //                DateTimePicker::make('two_factor_confirmed_at'),
             ]);
     }
 }
